c57c66d7dcf76d7a5be63a9210e705add63a2256
sugear <sugear@foxmail.com>
2014-12-17 16:51:23 +0800

    在同一个方法里用login/logout来测试多个用户的role

--- shiro-example-chapter3/src/test/java/com/github/zhangkaitao/shiro/chapter3/RoleTest2.java
+++ shiro-example-chapter3/src/test/java/com/github/zhangkaitao/shiro/chapter3/RoleTest2.java
@@ -10,6 +10,8 @@ import java.util.Arrays;
  * <p>
  * User: Zhang Kaitao
  * <p>
+ * 用wang这个用户来做一次测试
+ * <p>
  * Date: 14-1-26
  * <p>
  * Version: 1.0
@@ -22,7 +24,7 @@ public class RoleTest2 extends BaseTest {
         //判断拥有角色：role1
         Assert.assertTrue(subject().hasRole("role1"));
 
-        //判断拥有角色：role1 and role2 and !role3
+        //判断拥有角色：role1 and !role2 and !role3
         boolean[] result = subject().hasRoles(Arrays.asList("role1", "role2", "role3"));
         Assert.assertEquals(true, result[0]);
         Assert.assertEquals(false, result[1]);
@@ -35,6 +37,15 @@ public class RoleTest2 extends BaseTest {
         login("classpath:shiro-role.ini", "wang", "123");
         //断言拥有角色：role1
         subject().checkRole("role1");
+        
+        subject().logout();
+        
+        //换一个用户zhang来测试
+        login("classpath:shiro-role.ini", "zhang", "123");
+        //断言拥有角色：role1
+        subject().checkRole("role1");
+        
+        subject().logout();
 
     }
 
